5ffea880ac90127fd6c28bde589131bd2c651240
Ruskin Constant <ruskin.constant+dev@gmail.com>
2014-10-02 18:58:25 -0700

    mock tables, about to pivot structure of dashboard

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -56,34 +56,40 @@ end
 
 # view stems
 get '/stems' do
+	content_type :json
 	Stem.all.to_json
 end
 
 # view my stems
 get '/stems/producer/:id' do
+	content_type :json
 	Stem.all.where(producer: params[:producer]).to_json
 end
 
 #----------- Circles -----------
 # create circle
 post '/add_circle' do
+	content_type :json
 	Circle.create(params)
 	Circle.last.to_json
 end
 
 # view circles
 get '/circles' do
+	content_type :json
 	Circle.all.to_json
 end
 
 # submit stem to circle
 post '/circles/:id/stems/:id' do
+	content_type :json
 	add_stem = Circle.find(params[:circle_id]).stems.create(params[:stem])
 	add_stem.to_json
 end
 
 # mixup a circle, allocate remixes
 post '/circles/:id/mixup' do
+	content_type :json
 	circle = Circle.find(params[:circle_id])
 	mixup(circle).to_json
 end
@@ -95,5 +101,6 @@ end
 
 # view remixes
 get '/remixes' do
+	content_type :json
 	Remix.all.to_json
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
   $('button').on('click', function(event) {
   	event.preventDefault();
-  	console.log('adding a new stem');
-  	addStem();
+  	switch(event.target.className) {
+  		case 'add_stem':
+  			console.log('trying to add a stem');
+  			addStem()
+  			break;
+  		case 'add_circle':
+  			console.log('trying to add a circle');
+  			addCircle()
+  			break;
+  	}
   });
 });
 
@@ -26,7 +34,32 @@ function addStem(event) {
 
 function renderStem(Stem) {
 	console.log(Stem);
-	debugger
 	var stemRow = '<tr id="'+Stem.id+ '"><td>'+Stem.track_name+'</td><td>'+Stem.download_url+'</td><td><input type="button" value="update"><input type="button" value="delete"></td></tr>';
 	$('#my_stems').append(stemRow)
 }
+
+function addCircle(event) {
+	$.ajax({
+		url: '/add_circle',
+		type: 'POST',
+		data: $('#add_circle').serialize(),
+	})
+	.done(function(data) {
+		renderCircle(data);
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+}
+
+function renderCircle(circle) {
+	console.log(circle);
+	var circleRow = '<tr id="'+circle.id+ '"><td>'+circle.name+'</td><td>'+circle.signup_deadline+'</td>  <td>'+circle.submit_deadline+'</td> <td>'+0+' remixes so far</td> <td><input type="button" value="update"><input type="button" value="delete"></td></tr>';
+	$('#my_circles').append(circleRow)
+}
+	
+	
